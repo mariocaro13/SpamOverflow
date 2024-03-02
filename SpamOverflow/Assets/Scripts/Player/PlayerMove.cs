c1c9fdfa5db353c49b965aad067f74970aa15839
VillaBillings <129852795+VillaBillings@users.noreply.github.com>
2024-03-02 15:05:29 +0100

    Player Prefab Changes

--- SpamOverflow/Assets/Scripts/Player/PlayerMove.cs
+++ SpamOverflow/Assets/Scripts/Player/PlayerMove.cs
@@ -9,16 +9,15 @@ public class PlayerMove : MonoBehaviour
 	public Transform groundCheck;
 	private LayerMask groundLayer;
 	private LayerMask plataformLayer;
-	private LayerMask playerLayer;
 
 	private float horizontalInput;
+	private float verticalInput;
 	public float speed = 8f;
-	public float jumpForce = 2.5f;
+	public float jumpForce;
 	public float coyoteTime = 0.15f;
 	private float groundCheckRadius = 0.025f;
 	private float coyoteTimeCounter;
 	public float jumpBufferTime = 0.15f;
-	private float jumpBufferCounter;
 
 	private Vector2 movement;
 
@@ -31,88 +30,66 @@ public class PlayerMove : MonoBehaviour
 		rb = GetComponent<Rigidbody2D>();
 		groundLayer = LayerMask.GetMask("Ground");
 		plataformLayer = LayerMask.GetMask("OneWayPlataform");
-		playerLayer = LayerMask.GetMask("Player");
 	}
 
 	void Update()
 	{
 		horizontalInput = Input.GetAxisRaw("Horizontal");
-		float verticalInput = Input.GetAxisRaw("Vertical");
+		verticalInput = Input.GetAxisRaw("Vertical");
 		movement = new Vector2(horizontalInput, verticalInput);
 
 		_isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer | plataformLayer);
 
 		if (_isGrounded == true)
-		{
 			coyoteTimeCounter = coyoteTime;
-		}
 		else
-		{
 			coyoteTimeCounter -= Time.deltaTime;
-		}
 
-		if (Input.GetButtonDown("Jump") && !(Input.GetAxisRaw("Vertical") < 0))
-		{
-			jumpBufferCounter = jumpBufferTime;
-		}
+		if (Input.GetButtonDown("Jump") && verticalInput >= 0)
+			jumpBufferTime = 0.15f;
 		else
-		{
-			jumpBufferCounter -= Time.deltaTime;
-		}
+			jumpBufferTime -= Time.deltaTime;
 
-		if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
+		if (jumpBufferTime > 0f && coyoteTimeCounter > 0f) 
 		{
 			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
 			coyoteTimeCounter = 0f;
 		}
 
-		// Jump more ore less if hold (Como funciona???)
+		//Jump more or less
 		if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
 		{
-			rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-
+			rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 2);
 			coyoteTimeCounter = 0f;
 		}
 
-		if (horizontalInput < 0f && _facingRight == false)
-		{
-			Flip();
-		}
-		else if (horizontalInput > 0f && _facingRight == true)
-		{
-			Flip();
-		}
+		Flip();
 
-		if (Input.GetAxisRaw("Vertical") < 0 && Input.GetButtonDown("Jump") && _isGrounded && !_isPassingThrough)
-		{
+		if (verticalInput < 0 && Input.GetButtonDown("Jump") && _isGrounded && !_isPassingThrough)
 			StartCoroutine(AllowPassThrough());
-		}
-
 	}
 	private IEnumerator AllowPassThrough()
 	{
 		_isPassingThrough = true;
 		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("OneWayPlataform"), true);
+
 		yield return new WaitForSeconds(0.3f); // Espera 0.5 segundos
+
 		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("OneWayPlataform"), false);
 		_isPassingThrough = false;
 	}
 
 	private void Flip()
 	{
-		_facingRight = !_facingRight;
-		float localScaleX = transform.localScale.x;
-		localScaleX = localScaleX * 1f;
-		transform.localScale = new Vector3(localScaleX * -1f, transform.localScale.y, transform.localScale.z);
+		if (horizontalInput < 0f && !_facingRight || horizontalInput > 0f && _facingRight)
+		{
+			_facingRight = !_facingRight;
+			transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+		}		
 	}
 
 	private void FixedUpdate()
 	{
-		float horizontalVelocity = movement.normalized.x * speed;
-		float verticalVelocity = movement.normalized.y * speed;
-
-		// Por ahora, ya que habria que añadir la vel de plataforma si hubise
-		rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+		rb.velocity = new Vector2(movement.normalized.x * speed, rb.velocity.y);
 	}
 }
