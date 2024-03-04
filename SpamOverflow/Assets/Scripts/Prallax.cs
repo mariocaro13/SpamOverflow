d6ef03f9f722af2637d1c3ecb4b1db3706b8887e
VillaBillings <129852795+VillaBillings@users.noreply.github.com>
2024-03-04 07:06:03 +0100

    Edit Niveles y cosas varias

---
+++ SpamOverflow/Assets/Scripts/Prallax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prallax : MonoBehaviour
+{
+	private float startpos;
+	public GameObject play;
+	public float parallaxEffect;
+	public float length;
+
+	void Start()
+	{
+		startpos = transform.position.x;
+		//length = GetComponent<TilemapRenderer>().bounds.size.x;
+	}
+
+	void Update()
+	{
+		float temp = (play.transform.position.x * (1 - parallaxEffect));
+		float dist = (play.transform.position.x * parallaxEffect);
+
+		transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+		if (temp > startpos + length) startpos += length;
+		else if (temp < startpos - length) startpos -= length;
+	}
+}
